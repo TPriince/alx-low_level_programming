ead1c4c9f3d71ce394eaf094cc4b0e6f796c200a
TPriince <tobiloba110@gmail.com>
2022-03-12 08:22:57 -0800

    Program that prints all possible commbinations of single-digit numbers in ascending order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of a single
+ * number, in ascending order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
