b15ad5c50015f7802f1bdeffc72fc192bb7abf3c
TPriince <tobiloba110@gmail.com>
2022-04-21 13:45:25 -0700

    Task 0 - Function that prints all the elements of a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,28 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 1;
+	size_t nelem;
 
-	/* return 0 as number of nodes */
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	nelem = 0;
+	while (h != NULL)
 	{
-		/* if str is NULL return len as 0 and str as (nil) */
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		/* go to the next node */
 		h = h->next;
-		/* count numver of nodes */
-		node_count += 1;
+		nelem++;
 	}
-
-	/* print for the lasr node */
-	printf("[%d} %s\n", h->len, h->str);
-
-	return (node_count);
+	return (nelem);
 }
