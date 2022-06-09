f9d815e801a515c84b2c3a1d3cfaf44e5819d25a
TPriince <tobiloba110@gmail.com>
2022-06-09 08:51:34 -0700

    :heavy_check_mark: Task 1 - function that returns the number of elements in a linked dlistint_t list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -12,7 +12,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
@@ -22,6 +22,7 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 
 #endif /* LISTS_H */
